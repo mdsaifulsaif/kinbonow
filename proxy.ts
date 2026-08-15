8bcc928e6b1aa87450e46fe1d8b69ae56c424bd7
Saiful Saif <mdsaifulislam02137@gmail.com>
2026-08-15 10:20:00 +0600

    ok

--- proxy.ts
+++ proxy.ts
@@ -1,34 +1,7 @@
-// import { getToken } from "next-auth/jwt";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function middleware(req: NextRequest) {
-//   const token = await getToken({ 
-//     req, 
-//     secret: process.env.NEXTAUTH_SECRET 
-//   });
-  
-//   const { pathname } = req.nextUrl;
 
 
-//   if (pathname.startsWith("/dashboard")) {
-
-//     if (!token) {
-//       return NextResponse.redirect(new URL("/login", req.url));
-//     }
 
 
-//     if (pathname.startsWith("/dashboard/admin") && token.role !== "admin") {
-//       return NextResponse.redirect(new URL("/", req.url)); 
-//     }
-//   }
-
-//   return NextResponse.next();
-// }
-
-
-// export const config = {
-//   matcher: ["/dashboard/:path*"],
-// };
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -70,3 +43,4 @@ export async function proxy(req: NextRequest) {
 export const config = {
   matcher: ["/dashboard/:path*"],
 };
+
